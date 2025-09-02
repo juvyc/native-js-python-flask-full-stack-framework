80b19961201659fff43ca1ac9d7f6a34f506a3a0
JuvyC <cagapejuvy@gmail.com>
2025-09-02 22:06:49 +0800

    Security testing

--- templates/basic/fetches/admin/dashboard.py
+++ templates/basic/fetches/admin/dashboard.py
@@ -2,8 +2,6 @@ from flask import request
 import auth
 
 def __init__(params, render=False):
-    print(request.method)
-
     #JS Checker method if current user is logged-in
     if request.method == 'PATCH':
         return auth.exec()
